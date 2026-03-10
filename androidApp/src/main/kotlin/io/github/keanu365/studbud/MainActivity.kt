c19251e1e8c7c55c327bad1059cc2a76b2df3007
Keanu <mgkeanu@gmail.com>
2026-03-10 18:09:22 +0800

    Project Clean-up

--- androidApp/src/main/kotlin/io/github/keanu365/studbud/MainActivity.kt
+++ androidApp/src/main/kotlin/io/github/keanu365/studbud/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import io.github.keanu365.studbud.theme.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
